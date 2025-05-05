34e74cd0e24bbffbb4b0d0dbd095d6e1e06d3a47
Никита Павловский <nikitapavlovskiy2006@gmail.com>
2025-05-05 18:00:30 +0300

    • init

---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module com.nikitos.homeflow {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.nikitos.homeflow to javafx.fxml;
+    exports com.nikitos.homeflow;
+}
