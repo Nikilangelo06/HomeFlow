2fd742830a0d73e79825432b4ccacad9e10f9317
Никита Павловский <nikitapavlovskiy2006@gmail.com>
2025-05-06 23:12:07 +0300

    • Added Add Worker Button

--- src/main/java/com/nikitos/homeflow/Controller/OperatorMainController.java
+++ src/main/java/com/nikitos/homeflow/Controller/OperatorMainController.java
@@ -1,5 +1,6 @@
 package com.nikitos.homeflow.Controller;
 
+import com.nikitos.homeflow.Util.AlertHelper;
 import com.nikitos.homeflow.Util.PageHelper;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -8,5 +9,10 @@ public class OperatorMainController {
     @FXML
     protected void handleClickBackButton(ActionEvent event) {
         PageHelper.goToPage("password_operator_page.fxml", event);
+    }
+
+    @FXML
+    protected void handleClickAddWorker(ActionEvent event) {
+        AlertHelper.showAlert("Ага");
     }
 }
