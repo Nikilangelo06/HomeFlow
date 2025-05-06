d77b09543b378cf0aa073717289458bdc227cb1b
Никита Павловский <nikitapavlovskiy2006@gmail.com>
2025-05-06 19:31:48 +0300

    • Added logic for password Enter

--- src/main/java/com/nikitos/homeflow/Controller/PasswordOperatorController.java
+++ src/main/java/com/nikitos/homeflow/Controller/PasswordOperatorController.java
@@ -1,11 +1,16 @@
 package com.nikitos.homeflow.Controller;
 
 import com.nikitos.homeflow.Util.AlertHelper;
+import com.nikitos.homeflow.Util.Config;
 import com.nikitos.homeflow.Util.PageHelper;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 
 public class PasswordOperatorController {
+    @FXML
+    private TextField password_TextField;
+
     @FXML
     protected void handleClickBackButton(ActionEvent event) {
         PageHelper.goToPage("login_page.fxml", event);
@@ -13,6 +18,11 @@ public class PasswordOperatorController {
 
     @FXML
     protected void handleClickEnterButton(ActionEvent event) {
-        PageHelper.goToPage("operator_main_page.fxml", event);
+        if (password_TextField.getText().equals(Config.passwordForOperator)) {
+            PageHelper.goToPage("operator_main_page.fxml", event);
+        }
+        else {
+            AlertHelper.showAlert("Неверный пароль!");
+        }
     }
 }
---
+++ src/main/java/com/nikitos/homeflow/Util/Config.java
@@ -0,0 +1,6 @@
+package com.nikitos.homeflow.Util;
+
+public class Config {
+    // Пароль для входа как оператор
+    public static String passwordForOperator = "123";
+}
