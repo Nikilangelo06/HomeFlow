8e0a287bbed4d2ac2c677b92cf9cf58cf9f7aa3f
Никита Павловский <nikitapavlovskiy2006@gmail.com>
2025-05-10 19:25:51 +0300

    • Added Logic for adding form in DB

--- src/main/java/com/nikitos/homeflow/Controller/FormCreationController.java
+++ src/main/java/com/nikitos/homeflow/Controller/FormCreationController.java
@@ -1,6 +1,8 @@
 package com.nikitos.homeflow.Controller;
 
 import com.nikitos.homeflow.MainApp;
+import com.nikitos.homeflow.Model.Form;
+import com.nikitos.homeflow.Model.FormDAO;
 import com.nikitos.homeflow.Util.AlertHelper;
 import com.nikitos.homeflow.Util.PageHelper;
 import javafx.event.ActionEvent;
@@ -9,11 +11,18 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
+import java.sql.SQLException;
+
+import static com.nikitos.homeflow.Util.AlertHelper.showAlert;
+
 public class FormCreationController {
+    @FXML
+    private Button addForm_Button;
     @FXML
     private CheckBox electric_CheckBox;
     @FXML
@@ -31,8 +40,34 @@ public class FormCreationController {
 
 
     @FXML
-    protected void handleSendRequest(ActionEvent event) {
-        AlertHelper.showAlert("Ваша заявка принята, ожидайте звонка от мастера.");
+    protected void handleSendRequest(ActionEvent event) throws SQLException {
+        // Проверка на корректность данных
+        if (!isDataOK()) {
+            return;
+        }
+
+        String who_is_needed = "";
+        if (gasman_CheckBox.isSelected()) { who_is_needed += "0"; }
+        if (plumber_CheckBox.isSelected()) { who_is_needed += "1"; }
+        if (fitter_CheckBox.isSelected()) { who_is_needed += "2"; }
+        if (electric_CheckBox.isSelected()) { who_is_needed += "3"; }
+
+        Form form = new Form(
+                address_TextField.getText(),
+                fullName_TextField.getText(),
+                telephone_TextField.getText(),
+                who_is_needed);
+
+        System.out.println(form.getAddress());
+        System.out.println(form.getFullName());
+        System.out.println(form.getPhoneNumber());
+        System.out.println(form.getWhoIsNeeded());
+
+        FormDAO.addForm(form);
+
+        //addForm_Button.setDisable(true);
+
+        showAlert("Ваша заявка принята, ожидайте звонка от мастера.");
     }
 
 
@@ -40,5 +75,28 @@ public class FormCreationController {
     protected void handleClickBackButton(ActionEvent event) {
         // Возвращаемся на страницу входа
         PageHelper.goToPage("login_page.fxml", event);
+    }
+
+
+    private boolean isDataOK() {
+        boolean isOK = true;
+
+        if(address_TextField.getText().isEmpty()) {
+            isOK = false;
+            showAlert("Заполните поле адреса!");
+        } else if (fullName_TextField.getText().isEmpty()) {
+            isOK = false;
+            showAlert("Заполните поле ФИО!");
+        } else if (telephone_TextField.getText().isEmpty()) {
+            isOK = false;
+            showAlert("Заполните поле телефонного номера!");
+        }
+
+        if (!electric_CheckBox.isSelected() && !fitter_CheckBox.isSelected() && !plumber_CheckBox.isSelected() && !gasman_CheckBox.isSelected()) {
+            isOK = false;
+            showAlert("Выберите хотя бы одного специалиста!");
+        }
+
+        return isOK;
     }
 }
